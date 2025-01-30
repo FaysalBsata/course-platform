920c0ccbb7f12b3198299b23622696a761d5767d
Faysal Bsata <faysalbsata@hotmail.com>
2025-01-30 22:19:12 +0400

    Add authentication pages and integrate Clerk for user management

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+export default {
+  content: [
+    './src/pages/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/components/**/*.{js,jsx,ts,tsx,mdx}',
+    './src/app/**/*.{js,jsx,ts,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        sm: '1500px',
+      },
+    },
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
