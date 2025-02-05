1de4de8d47d83c85d896be8954804bdd6496f3b3
Faysal Bsata <faysalbsata@hotmail.com>
2025-02-05 22:14:41 +0400

    Remove unused SVG files and delete obsolete course page; update popover component and add multi-select feature

--- src/features/products/components/ProductForm.tsx
+++ src/features/products/components/ProductForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import RequiredLabelIcon from '@/components/RequiredLabelIcon';
 import { Button } from '@/components/ui/button';
+import { MultiSelect } from '@/components/ui/custom/multi-select';
 import {
   Form,
   FormControl,
@@ -10,7 +11,15 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
+import { ProductStatus, productStatuses } from '@/drizzle/schema';
 import {
   createProduct,
   updateProduct,
@@ -22,14 +31,29 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 export default function ProductForm({
   product,
+  courses,
 }: {
-  product?: { id: string; name: string; description: string };
+  product?: {
+    id: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    status: ProductStatus;
+    coursesCount: number;
+    customersCount: number;
+    priceInDollars: number;
+  };
+  courses: { id: string; name: string }[];
 }) {
   const form = useForm<z.infer<typeof productSchema>>({
     resolver: zodResolver(productSchema),
     defaultValues: product ?? {
       name: '',
       description: '',
+      courseIds: [],
+      imageUrl: '',
+      priceInDollars: 0,
+      status: 'private',
     },
   });
   async function onSubmit(values: z.infer<typeof productSchema>) {
@@ -44,17 +68,112 @@ export default function ProductForm({
         onSubmit={form.handleSubmit(onSubmit)}
         className="flex gap-6 flex-col"
       >
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 items-start">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="priceInDollars"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Price
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    {...field}
+                    step={1}
+                    min={0}
+                    onChange={(e) =>
+                      field.onChange(
+                        isNaN(e.target.valueAsNumber)
+                          ? ''
+                          : e.target.valueAsNumber
+                      )
+                    }
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Image Url
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {productStatuses.map((status) => (
+                      <SelectItem key={status} value={status}>
+                        {status}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <FormField
           control={form.control}
-          name="name"
+          name="courseIds"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                <RequiredLabelIcon />
-                Name
-              </FormLabel>
+              <FormLabel>Included Courses</FormLabel>
               <FormControl>
-                <Input {...field} />
+                <MultiSelect
+                  selectPlaceholder="Select courses"
+                  searchPlaceholder="Search courses"
+                  options={courses}
+                  getLabel={(course) => course.name}
+                  getValue={(course) => course.id}
+                  selectedValues={field.value}
+                  onSelectedValuesChange={field.onChange}
+                />
               </FormControl>
               <FormMessage />
             </FormItem>
